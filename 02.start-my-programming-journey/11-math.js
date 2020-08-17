eb6181599325ce50048ef5ed29383da705f32e42
mkstamin <mkstamincse@gamil.com>
2020-08-17 21:49:46 +0600

    I have worked on some math and date function(11-math,12-date)

---
+++ 02.start-my-programming-journey/11-math.js
@@ -0,0 +1,17 @@
+console.log(Math.E);
+console.log(Math.PI);
+
+let n = 4.48
+console.log(Math.abs(n));
+console.log(Math.floor(n));
+console.log(Math.ceil(n));
+console.log(Math.round(n));
+
+console.log(Math.max(74, 35, 345, 643));
+console.log(Math.min(74, 35, 345, 643));
+
+console.log(Math.pow(2, 3));
+console.log(Math.sqrt(16));
+
+console.log(Math.random());
+console.log(Math.round(Math.random() * 50 + 1));
