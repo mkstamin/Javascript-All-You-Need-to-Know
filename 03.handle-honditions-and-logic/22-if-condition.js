55db3920b2c7c02afdd54b1bbd52ed9c837c1e29
mkstamin <mkstamincse@gamil.com>
2020-08-20 13:40:39 +0600

    I added 03 folder and work on 2 files 22-if-condition and 23-esle-condition

---
+++ 03.handle-honditions-and-logic/22-if-condition.js
@@ -0,0 +1,20 @@
+let a = 150
+let b = 20
+
+if (a > b) {
+    console.log(a + ' is getter then ' + b)
+}
+
+if (a < b) {
+    console.log(b + ' is getter then ' + a);
+}
+
+
+let n = 10
+
+if (n % 2 == 0) {
+    console.log(n + ' is a EVEN number');
+}
+if (n % 2 !== 0) {
+    console.log(n + ' is a ODD number');
+}
---
+++ 03.handle-honditions-and-logic/23-else-condition.js
@@ -0,0 +1,17 @@
+let a = 30
+let b = 20
+
+if (a > b) {
+    console.log('A is getter then B')
+} else {
+    console.log('B is getter then A');
+}
+
+
+let n = 11
+
+if (n % 2 == 0) {
+    console.log(n + ' is a EVEN number');
+} else {
+    console.log(n + ' is a ODD number');
+}
